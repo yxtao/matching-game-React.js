567b5181341365f52a00713b43fa9f5935cb9f13
yxtao <yinxiatao@gmail.com>
2021-07-01 16:28:35 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,102 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect, useReducer } from 'react';
 
+const Card = (props) =>{
+  const [num, setNum] = useState ("..."); // avoid changing pros.value or props.faceup
+  const parentCallback = () =>{
+     setNum(props.value);
+     props.callback(props.value);
+     setTimeout(()=> {setNum("...")}, 1000); // first input must be a function 
+  }
+  return (
+    <button className="gridItem" onClick = {parentCallback} disabled = {props.faceup}>  {props.faceup? props.value: num}  </button>
+  ) 
+}
+
+const Board = (props) =>{
+  const [cards, setCards]= useState(createCards(props.cardnums));
+  const [pairs, setPairs] = useState([]);
+  const [clickedCards, setClickedCards] = useState([])
+  const handleCallback = (num)=>{
+    setClickedCards((pre)=>  [...pre, num])
+    if(clickedCards.length === 1 ) {
+      if(clickedCards[0]===num) {
+       setPairs((prePairs)=> [...prePairs, num]); 
+      }
+       setClickedCards([]);
+    } 
+    console.log(clickedCards);
+    console.log(pairs)
+  }
+
+  useEffect(()=>{
+   // setCards((preCards)=>preCards.map((card)=> ({value:card.value, faceup: pairs.includes(card.value)? true : false} ) ))
+    setCards((preCards)=>preCards.map((card)=> ({
+                                               ...card,
+                                                faceup: pairs.includes(card.value)  } ) ))
+  },[pairs, clickedCards])
+
+ function createCards(nums){ 
+      var cardlist=[];
+      for (var i=0; i<nums.length; i++) {
+           var card = {value:nums[i], faceup: false };
+           cardlist.push(card)
+      }
+      return cardlist
+    }
+
+  return(
+    <div>
+      <div className= "gridContainer">
+        {cards.map((card)=> (<Card value={card.value} faceup={card.faceup} callback = {handleCallback} /> )) }
+        {cards.map((card)=> (<Card value={card.value} faceup={card.faceup} callback = {handleCallback} /> )) }
+      </div>
+    </div>
+  )
+}
+const Game = () =>{
+  const [start, setStart] = useState(false);
+
+  const initialState = {nums: [1,2,3]};
+  const reducer = (state, action) => {
+     switch(action.type) {
+      case 'add': 
+         return { nums: [...state.nums,Math.max(...state.nums)+1] };
+      case 'reduce' : 
+         return {nums:state.nums.filter((num)=> num!==Math.max(...state.nums) )};
+      default:
+        return state.nums   
+     }
+
+  }
+  const [state, dispatch] = useReducer (reducer, initialState)
+ 
+  const mystyle={
+    padding:"20px", 
+    fontSize: "30px" 
+  }
+  return (
+    <div className="App">
+      <div style={mystyle} >
+         current cards:  {state.nums} 
+      </div>
+      <div style={mystyle}>      
+        <button style={mystyle} disabled={start} onClick={()=> dispatch({type: 'add'})}>Add cards</button> 
+      </div>
+      <div style={mystyle}> 
+        <button style={mystyle} disabled={start} onClick={()=> dispatch({type: 'reduce'})}>Reduce cards</button> 
+      </div>
+       {start? <div style={mystyle}> <Board cardnums = {state.nums} /> <button style={mystyle} onClick = {()=> setStart(false)}> restart</button> </div>
+              : <button style={mystyle} onClick={()=> setStart(true)}> start </button> } 
+     
+    </div>
+  );
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Game />
     </div>
   );
 }
