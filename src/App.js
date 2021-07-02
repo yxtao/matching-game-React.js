30bce5b59324d31424da1da4ec4570130e25ce11
Tao <57578694+yxtao@users.noreply.github.com>
2021-07-01 19:59:52 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -24,10 +24,12 @@ const Board = (props) =>{
   const handleCallback = (data)=>{
     setClickedCards((pre)=>  [...pre, data])
     if(clickedCards.length === 1 ) {
-      if(clickedCards[0].value === data.value) {
-       setPairs((prePairs)=> [...prePairs, data.value]); 
-      }
+      if(clickedCards[0].id !== data.id) {
+            if(clickedCards[0].value === data.value) {
+            setPairs((prePairs)=> [...prePairs, data.value]); 
+            }
        setClickedCards([]);
+      }
     } 
     console.log(clickedCards);
     console.log(pairs)
