5d16cc19730fa8625d4eda423baa1ef3a4374c7e
Tao <57578694+yxtao@users.noreply.github.com>
2021-07-01 21:25:37 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -22,14 +22,12 @@ const Board = (props) =>{
   const [cards, setCards]= useState(createCards(props.cardnums));
   const [pairs, setPairs] = useState([]);
   const [clickedCards, setClickedCards] = useState([])
-  const  timer= setTimeout = (()=> {setSeconds((pre)=> pre+1)}, 1000);
-  useEffect(timer,[])                   
+   
   const handleCallback = (data)=> {
       if (clickedCards.length === 0) {
             setClickedCards((pre) => [...pre,data]);
          }
       else if(clickedCards.length === 1 ) {
-        // alert("length==1 :: " +clickedCards[0].value +"::"+data.value);
          if(clickedCards[0].id === data.id) return
          if(clickedCards[0].value === data.value) {
               setPairs((prePairs)=> [...prePairs, data.value]); 
